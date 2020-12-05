d01a63c66b6dd5ff8b80c84c8ba9f8c5dd6f9910
DESKTOP-MSIAly <ehab.ayman.gh@gmail.com>
2020-12-05 10:10:46 +0200

    Moving trash to player

--- Assets/Trash Spawner/TrashObject.cs
+++ Assets/Trash Spawner/TrashObject.cs
@@ -5,19 +5,54 @@ using UnityEngine;
 public class TrashObject : MonoBehaviour
 {
     [SerializeField] float itemWeight;
+    bool collected;
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
     void Start()
     {
-
+        collected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // transform.position = Vector3.Lerp(transform.position, Camera.main.transform.position, Time.deltaTime);
+        if (collected)
+        {
 
-    }
+        }
+            //    //transform.position = Vector3.Lerp(transform.position, Camera.main.transform.position, Time.deltaTime);
+            //    //if (transform.position == Camera.main.transform.position)
+            //    //{
+            //    //    Debug.Log("t");
+            //    //    collected = false;
+            //    //}
+            //    StartCoroutine(MoveToCamera());
+            //    collected = false;
+            //}
+
+        }
     void OnMouseDown()
     {
         ScoreManager.Instance.AddToScore(itemWeight);
-        Destroy(gameObject);
+        collected = true;
+        StartCoroutine(MoveToCamera());
+    }
+    IEnumerator MoveToCamera()
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed < 0.2)
+        {
+            transform.position = Vector3.Lerp(gameObject.transform.position, Camera.main.transform.position, timeElapsed / 0.2f);
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed > 0.1f)
+                Destroy(gameObject);
+            yield return null;
+        }
+        transform.position = Camera.main.transform.position;
     }
 }
